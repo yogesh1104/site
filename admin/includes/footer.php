33133d297bbea17789bf0f050e2409d59d33de35
alexbratu <alexbratuu@gmail.com>
2015-09-28 20:27:42 +0300

    products
    produccts

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,6 +2,20 @@
         
     <footer class="text-center" id="footer">&copy; Copyright 2015 Music Gym</footer>
         
-   
+    <script>
+        function get_child_options(){
+            var parentID = $('#parent').val();
+            $.ajax({
+                url: '/site/admin/parsers/child_categories.php',
+                type: 'POST',
+                data: {parentID: parentID},
+                success: function(data){
+                    $('#child').html(data);
+                },
+                error: function(){alert("Something went wrong with the child options.")},
+            });
+        }
+        $('select[name="parent"]').change(get_child_options);
+    </script>
     </body> 
 </html>
--- admin/products.php
+++ admin/products.php
@@ -2,6 +2,71 @@
 require_once $_SERVER['DOCUMENT_ROOT'].'/site/core/init.php';
     include 'includes/head.php';
     include 'includes/navigation.php';
+    if(isset($_GET['add'])) {
+    $brandQuery = $db->query("SELECT * FROM brand ORDER BY brand");
+    $parentQuery = $db->query("SELECT * FROM categories WHERE parent = 0 ORDER BY category");
+        ?>
+    <h2 class="text-center">Add a new product</h2><hr>
+    <form action="products.php?add=1" method="POST" enctype="multipart/form-data">
+        <div class="form-group col-md-3">
+            <label for="title">Title*: </label>
+            <input type="text" name="title" class="form-control" id="title" value="<?=((isset($_POST['title']))?sanitize($_POST['title']):''); ?>">
+        </div>
+        <div class="form-group col-md-3">
+            <label for="brand">Brand*:</label>
+            <select class="form-control" id="brand" name="brand">
+                <option value=""<?=((isset($_POST['brand']) && $_POST['brand'] == '')?' selected':''); ?>></option>
+                <?php while($brand = mysqli_fetch_assoc($brandQuery)): ?>
+                    <option value="<?=$brand['id'];?>"<?=((isset($_POST['brand']) && $_POST['brand']==$brand['id'])?' selected':'');?>><?= $brand['brand']; ?></option>
+                
+                <?php endwhile; ?>
+            </select>
+        </div>
+        <div class="form-group col-md-3">
+            <label for="parent">Parent Category*:</label>
+            <select class="form-control" id="parent" name="parent">
+                <option value=""<?=((isset($_POST['parent']) && $_POST['parent']=='')?' selected':'');  ?>></option>
+                <?php while($parent = mysqli_fetch_assoc($parentQuery)): ?>
+                    <option value="<?= $parent['id'];?>"<?= ((isset($_POST['parent']) && $_POST['parent'] == $parent['id'])?' select': ''); ?>><?= $parent['category']; ?></option>
+                <?php endwhile; ?>
+            </select>
+        </div>
+        <div class="form-group col-md-3">
+            <label for="child">Child Category*:</label>
+            <select id="child" name="child" class="form-control">
+            </select>
+        </div>
+        <div class="form group col-md-3">
+            <label for="price">Price*:</label>
+            <input type="text" id="price" name="price" class="form-control" value=""<?=((isset($_POST['price']))?sanitize($_POST['price']):''); ?>></input>
+        
+        </div>
+        <div class="form group col-md-3">
+            <label for="price">List Price*:</label>
+            <input type="text" id="list_price" name="list_price" class="form-control" value=""<?=((isset($_POST['list_price']))?sanitize($_POST['list_price']):''); ?>></input>
+        </div>
+        <div class = "form-group col-md-3">
+            <label>Quantity*:</label>
+            <button class="btn btn-default form-control" onclick="$('#quantityModal').modal('toggle'); return false;">Quantity</button>
+        </div>
+        <div class="form-group col-md-3">
+            <label for="quantity">Quantity Preview</label>
+            <input class="form-control" type="text" name="quantity" id="quantity" value="" readonly>
+        </div>
+        <div class="form-group col-md-6">
+            <label for="photo">Product Photo:</label>
+            <input type="file" name="photo" id="photo" class="form-control">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="description">Description:</label>
+            <textarea id="description" name="description" class="form-control" rows="6"><?= ((isset($_POST['description']))?sanitize($POST['description']):'');?></textarea>
+        </div>
+        <div class="form-group pull-right">
+            <input type="submit" value="Add product" class="form-control btn btn-success">
+        </div><div class="clearfix"></div>    
+    </form>
+    
+    <?php }else{
     $sql = "SELECT * FROM products WHERE deleted = 0";
     $presults = $db->query($sql);
     if(isset($_GET['featured'])){
@@ -52,4 +117,4 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/site/core/init.php';
 
 
 
-<?php include 'includes/footer.php' ?>
+<?php } include 'includes/footer.php' ?>
