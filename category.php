e2b8ec47c456e924eafa399e87414e1ea34b98dd
alexbratu <alexbratuu@gmail.com>
2015-11-07 19:44:49 +0200

    categories

---
+++ category.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'core/init.php';
+include 'includes/head.php'; 
+include 'includes/navigation.php';
+include 'includes/headerpartial.php';
+include 'includes/leftbar.php';
+
+if(isset($_GET['cat'])){
+    $cat_id = sanitize($_GET['cat']);   
+}else{
+    $cat_id='';
+}
+
+$sql = "SELECT * FROM  products WHERE categories = '$cat_id'";
+$productQuery = $db->query($sql);
+$category = get_category($cat_id);
+
+?>
+                        
+            <!-- Main Content -->
+            <div class="col-md-8">
+                <div class="row"> 
+                    <h2 class="text-center"><?=$category['child']; ?></h2>
+                    <?php while($product = mysqli_fetch_assoc($productQuery)) : ?>
+                        <div class="col-md-3">
+                            <h4><?= $product['title']; ?></h4>
+                            <img src="<?= $product['image'];?>" alt="<?= $product['title']; ?>" class="img-thumb"/>
+                            <p class="list-price text-danger">List Price <s><?= $product['list_price'];?> &euro;</s></p>
+                            <p class="price">Our Price: <?= $product['price'];?> &euro;</p>
+                            <button type="button" class="btn btn-sm btn-success" onclick="detailsmodal(<?= $product['id']; ?>)" >Details</button>
+                        </div>
+                    <?php endwhile; ?>
+                   
+                </div>
+            </div>
+
+<?php
+//include 'includes/detailsmodal.php';
+include 'includes/rightbar.php';
+include 'includes/footer.php';
+?>       
---
+++ includes/headerpartial.php
@@ -0,0 +1,8 @@
+ <!-- Header -->
+        <div id="partial-headerWrapper">
+            <div id="partial-back"></div>
+            <div id="partial-logotext"></div>
+            <div id="partial-fore"></div>
+        </div>
+        
+        <div class="container-fluid">
